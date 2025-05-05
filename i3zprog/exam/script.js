2f72c6952b2964ab251d578aeede9c2ec4c155f6
3z <195738687+i3zprog@users.noreply.github.com>
2025-05-05 23:19:52 +0300

    ex 2

---
+++ i3zprog/exam/script.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+function myinfo() {
+    // تعريف المتغيرات
+    const name = "عبدالرحمن الأحمد";
+    const age = 30;
+
+    // عرض المعلومات في الكونسول
+    console.log("الاسم: " + name);
+    console.log("العمر: " + age);
+}
+
+
+
+function welcomeButton(hello) {
+    alert("مرحبا بك في سيرتي الذاتية");
+    
+}
+
+function randommsg() {
+    // عرض رسالة في الكونسول
+    console.log("هذه رسالة تم عرضها في الكونسول!");
+}
+
+
+
